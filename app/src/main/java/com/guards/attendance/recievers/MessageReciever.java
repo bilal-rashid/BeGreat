8af0518770073ca7afd1217e141d4f536636ca9a
Bilal rashid <bilal.rashid@hypernymbiz.com>
2018-02-05 15:17:38 +0500

    Admin check added in receiver

--- app/src/main/java/com/guards/attendance/recievers/MessageReciever.java
+++ app/src/main/java/com/guards/attendance/recievers/MessageReciever.java
@@ -10,13 +10,13 @@ import android.util.Log;
 
 import com.guards.attendance.FrameActivity;
 import com.guards.attendance.enumerations.StatusEnum;
-import com.guards.attendance.fragments.AlarmFragment;
 import com.guards.attendance.fragments.EmergencyFragment;
 import com.guards.attendance.models.Packet;
 import com.guards.attendance.utils.ActivityUtils;
 import com.guards.attendance.utils.AppUtils;
 import com.guards.attendance.utils.Constants;
 import com.guards.attendance.utils.GsonUtils;
+import com.guards.attendance.utils.LoginUtils;
 
 import static android.content.Context.POWER_SERVICE;
 
@@ -35,14 +35,16 @@ public class MessageReciever extends BroadcastReceiver {
             if (message.contains("\"" + Constants.UNIQUE_ID + "\"")) {
                 Packet packet = GsonUtils.fromJson(message, Packet.class);
                 if (packet.status.equals(StatusEnum.EMERGENCY.getName())) {
-                    Log.d("TAAAG","emergency");
-                    PowerManager.WakeLock screenLock = ((PowerManager)context.getSystemService(POWER_SERVICE)).newWakeLock(
-                            PowerManager.SCREEN_BRIGHT_WAKE_LOCK| PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
-                    screenLock.acquire(5000);
-                    AppUtils.vibrate(context);
-                    Bundle bundle2 = new Bundle();
-                    bundle2.putString(Constants.GUARD_DATA, message);
-                    ActivityUtils.startAlarmActivity(context, FrameActivity.class, EmergencyFragment.class.getName(),bundle2,false);
+                    if(LoginUtils.isAdminUserLogin(context)) {
+                        Log.d("TAAAG", "emergency");
+                        PowerManager.WakeLock screenLock = ((PowerManager) context.getSystemService(POWER_SERVICE)).newWakeLock(
+                                PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+                        screenLock.acquire(5000);
+                        AppUtils.vibrate(context);
+                        Bundle bundle2 = new Bundle();
+                        bundle2.putString(Constants.GUARD_DATA, message);
+                        ActivityUtils.startAlarmActivity(context, FrameActivity.class, EmergencyFragment.class.getName(), bundle2, false);
+                    }
                 }
             }
         }
