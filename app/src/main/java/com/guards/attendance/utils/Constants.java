c96ecee7216b2fd75ab592cd97624d2af4c1cb2e
bilal-rashid <bilalrashid1992@gmail.com>
2018-03-03 18:05:25 +0500

    identifier changed

--- app/src/main/java/com/guards/attendance/utils/Constants.java
+++ app/src/main/java/com/guards/attendance/utils/Constants.java
@@ -17,8 +17,8 @@ public class Constants {
     public static final String CHECKIN = "checkin";
     public static final String CHECKOUT = "checkout";
     public static final String SUPERVISOR_CHECKIN = "supervisor_checkin";
-    public static final String UNIQUE_ID_GUARD = "G2m-Y4-K5";
-    public static final String UNIQUE_ID_SUPERVISOR = "P2L-p4-S7";
+    public static final String UNIQUE_ID_GUARD = "G2-Y6K5";
+    public static final String UNIQUE_ID_SUPERVISOR = "P2-L8S7";
     public static final long INTERVAL = 1000 * 60 * 60;
     public static final String GUARD_DATA = "guard_data";
 
